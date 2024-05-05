ee7010abe8d18bde7e4e6419a71814e61bc332d5
Aman Goel <amangoel9873572693@gmail.com>
2024-05-05 23:36:26 +0530

    5th May 2024: Completed creating shapes in swiftUI

---
+++ SwiftUILearning/SwiftUILearning/ShapesBootcamp.swift
@@ -0,0 +1,42 @@
+//
+//  ShapesBootcamp.swift
+//  SwiftUILearning
+//
+//  Created by aman on 05/05/24.
+//
+
+import SwiftUI
+
+struct ShapesBootcamp: View {
+    var body: some View {
+//        Circle()
+//            .fill(Color.blue)
+//            .foregroundColor(.pink)
+//            .stroke()
+//            .stroke(Color.orange)
+//            .stroke(Color.pink, lineWidth: 3.0)
+//            .stroke(Color.orange, style: StrokeStyle(lineWidth: 20, lineCap: .round, dash: [30]))
+//            .trim(from: 0.2, to: 1.0)
+//            .stroke(Color.purple, lineWidth: 50)
+        
+        
+        
+//        Ellipse()
+//            .frame(width: 200, height: 100)
+        
+//        Capsule(style: .continuous)
+        
+//        Rectangle()
+//            .frame(width: 100, height: 100)
+            
+//        
+//        RoundedRectangle(cornerRadius: 25.0)
+//            .frame(width: 100, height: 100)
+//        
+        
+    }
+}
+
+#Preview {
+    ShapesBootcamp()
+}
