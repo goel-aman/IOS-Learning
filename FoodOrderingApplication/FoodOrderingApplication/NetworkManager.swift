46bcd8f1dfacf2c56e3a46d0c560f816f9a46f47
Aman Goel <amangoel9873572693@gmail.com>
2024-06-06 08:39:34 +0530

    6th June 2024: SwiftUI Learnings.

--- FoodOrderingApplication/FoodOrderingApplication/NetworkManager.swift
+++ FoodOrderingApplication/FoodOrderingApplication/NetworkManager.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import UIKit
 
 final class networkManager {
     
     static let shared = networkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers"
@@ -53,4 +55,31 @@ final class networkManager {
         
         task.resume()
     }
+    
+    func downloadImage(fromURLString urlString: String, completed: @escaping (UIImage?) -> Void) {
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            
+            
+        }
+        
+    }
+    
+    
 }
