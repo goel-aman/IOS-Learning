f3c10d40518cdd2475eeb4ea305d22c0ce40f3d2
Aman <aman.goel@vestaeldercare.com>
2024-06-12 11:23:33 +0530

    12th June swiftUI learning

--- FoodOrderingApplication/FoodOrderingApplication/AccountView.swift
+++ FoodOrderingApplication/FoodOrderingApplication/AccountView.swift
@@ -8,27 +8,37 @@
 import SwiftUI
 
 struct AccountView: View {
+    @StateObject var viewModel = AccountViewModel()
     
-    @State private var firstName = ""
-    @State private var lastName = ""
-    @State private var email = ""
-    @State private var birthdate = Date()
+ 
     
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("Personal Info")) {
-                    TextField("First Name", text: $firstName)
-                    TextField("Last Name", text: $lastName)
-                    TextField("Email ", text: $email)
+                    TextField("First Name", text: $viewModel.firstName)
+                    TextField("Last Name", text: $viewModel.lastName)
+                    TextField("Email ", text: $viewModel.email)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .disableAutocorrection(false)
-                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    DatePicker("Birthday", selection: $viewModel.birthdate, displayedComponents: .date)
                     
                     
-                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
                 }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $viewModel.extraNapkins)
+                    
+                    
+                    Toggle("Frequent Refills", isOn: $viewModel.frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
             }
             .navigationTitle("Account")
         }
@@ -38,3 +48,4 @@ struct AccountView: View {
 #Preview {
     AccountView()
 }
+ 
