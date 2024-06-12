3a253793b2b0ab60ebaa8af23283fad72d34b84a
Aman <aman.goel@vestaeldercare.com>
2024-06-12 17:48:47 +0530

    12th June 2024

--- FoodOrderingApplication/FoodOrderingApplication/Accounts/AccountView.swift
+++ FoodOrderingApplication/FoodOrderingApplication/Accounts/AccountView.swift
@@ -16,31 +16,34 @@ struct AccountView: View {
         NavigationView {
             Form {
                 Section(header: Text("Personal Info")) {
-                    TextField("First Name", text: $viewModel.firstName)
-                    TextField("Last Name", text: $viewModel.lastName)
-                    TextField("Email ", text: $viewModel.email)
+                    TextField("First Name", text: $viewModel.user.firstName)
+                    TextField("Last Name", text: $viewModel.user.lastName)
+                    TextField("Email ", text: $viewModel.user.email)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .disableAutocorrection(false)
-                    DatePicker("Birthday", selection: $viewModel.birthdate, displayedComponents: .date)
+                    DatePicker("Birthday", selection: $viewModel.user.birthdate, displayedComponents: .date)
                     
                     
                     Button {
-                        print("Save")
+                        viewModel.saveChanges()
                     } label: {
                         Text("Save Changes")
                     }
                 }
                 
                 Section(header: Text("Requests")) {
-                    Toggle("Extra Napkins", isOn: $viewModel.extraNapkins)
+                    Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
                     
                     
-                    Toggle("Frequent Refills", isOn: $viewModel.frequentRefills)
+                    Toggle("Frequent Refills", isOn: $viewModel.user.frequentRefills)
                 }
                 .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
             }
             .navigationTitle("Account")
+        }
+        .alert(item: $viewModel.alertItem) { alertItem in
+            Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
         }
     }
 }
--- FoodOrderingApplication/FoodOrderingApplication/Common/Alert.swift
+++ FoodOrderingApplication/FoodOrderingApplication/Common/Alert.swift
@@ -24,4 +24,13 @@ struct AlertContext {
     static let invalidURL = AlertItem(title: Text("Server Error"), message: Text("There was an issue connecting to the server. If this persists, please contact support."), dismissButton: .default(Text("OK")))
     
     static let unableToComplete = AlertItem(title: Text("Server Error"), message: Text("unable to complete your request. Please check you network once."), dismissButton: .default(Text("OK")))
+    
+    //Mark: - Accounts Alert
+    static let invalidForm = AlertItem(title: Text("invalid Form"), message: Text("Please ensure all fields in the form have been filled out."), dismissButton: .default(Text("OK")))
+    
+    static let invalidEmail = AlertItem(title: Text("invalid Email"), message: Text("Please ensure your email is correct"), dismissButton: .default(Text("OK")))
+    
+    static let userSaveSuccess = AlertItem(title: Text("Profile Saved"), message: Text("Your profile information was successfully saved") , dismissButton: .default(Text("OK")))
+    
+    static let invalidUserData = AlertItem(title: Text("Profile Error"), message: Text("There was an error saving or retrieving your profile."), dismissButton: .default(Text("OK")))
 }
