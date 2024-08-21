c26303b688e2dacfccf03ac9fa2cfc7e8c12eadf
Aman Goel <amangoel9873572693@gmail.com>
2024-08-21 15:28:27 +0530

    Update AppDelegate.swift

--- MapsIntegration/MapsIntegration/AppDelegate.swift
+++ MapsIntegration/MapsIntegration/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GMSPlacesClient.provideAPIKey("AIzaSyCbLyU6uhy37-iKaPCMb6o4RScr_yzyS14")
+        GMSPlacesClient.provideAPIKey("")
         
         
         return true
