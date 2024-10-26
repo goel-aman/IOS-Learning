802127f7abdce34631167ee3c098c4f82430addd
Aman Goel <amangoel9873572693@gmail.com>
2024-10-26 16:38:01 +0530

    26th October: iOS Learnings

--- AssociatedTypesInEnums.playground/Contents.swift
+++ AssociatedTypesInEnums.playground/Contents.swift
@@ -1,3 +1,10 @@
-import UIKit
+enum Digit: String {
+    case one = "1"
+    case two = "1234"
+}
 
-var greeting = "Hello, playground"
+let myDigit = Digit.one
+
+print(myDigit)
+print(Digit.one.rawValue)
+
